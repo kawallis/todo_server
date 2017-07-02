0c5ba43f8efec6128da7a84fd36fc0fcd3079710
Chris_Wallace <kawallis@icloud.com>
2017-07-02 11:24:29 -0700

    added boilerplate

---
+++ server.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-console */
+const app = require('./lib/app');
+const http = require('http');
+require('./lib/connect');
+
+const PORT = process.env.PORT || 4000;
+const server = http.createServer(app);
+
+server.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
